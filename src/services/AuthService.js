f68abee8e5aee2de7280c8797c338ca1f82d1454
pranjalvatsa <pranjalvatsa86@gmail.com>
2025-10-02 16:35:12 +0530

    Fix User model import in AuthService

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
-const { User } = require('../models');
+const User = require('../models/User');
 const winston = require('winston');
 
 class AuthService {
